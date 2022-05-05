833026a8a92490089f18279f2c8f2f03c9c8f19c
Michele Delledonne <m.delledonne002@unibs.it>
2022-05-05 16:55:42 +0200

    Ready to test

--- src/tree_test.cpp
+++ src/tree_test.cpp
@@ -1,13 +1,42 @@
+#include <ros/ros.h>
 #include <behaviortree_cpp_v3/bt_factory.h>
-#include <action_learning_tests/skills_classes.h>
+#include <skills_executer/bt_skills_classes.h>
+#include <kr_50_r2500_tests/bt_goto_class.h>
 
-int main()
+int main(int argc, char **argv)
 {
+    ros::init(argc, argv, "tree_test");
+    ros::NodeHandle nh("tree_test_server");
+
+    ROS_INFO("Tree test start");
+
     BT::BehaviorTreeFactory factory;
 
-    factory.registerNodeType<SkillActionNode>("SkillActionNode");
+    ROS_INFO("Factory created");
 
-    auto tree = factory.createTreeFromFile("~/projects/planning_ws/src/kr_50_r2500/kr_50_r2500_tests/trees/test_tree.xml");
+    factory.registerNodeType<SkillActionNode>("SkillActionNode");
+    ROS_INFO("SkillActionNode registered");
+
+    factory.registerNodeType<GoToActionNode>("GoToActionNode");
+    ROS_INFO("GoToActionNode registered");
+
+    ROS_INFO("Pre action tree");
+
+    BT::Tree action_tree = factory.createTreeFromFile("/home/gauss/projects/planning_ws/src/kr_50_r2500_tests/config/trees/action1_tree.xml");
+
+    ROS_INFO("Post action tree");
+
+    ROS_INFO("Pre tree");
+
+    BT::Tree tree = factory.createTreeFromFile("/home/gauss/projects/planning_ws/src/kr_50_r2500_tests/config/trees/main_tree.xml");
+
+    ROS_INFO("Post tree");
+
+//    BT::Tree tree = factory.createTreeFromFile("/home/gauss/projects/planning_ws/src/kr_50_r2500_tests/config/trees/test_tree.xml");
+
+    ROS_INFO("Tree created");
 
     tree.tickRoot();
+
+    ROS_INFO("Tree test finish");
 }
